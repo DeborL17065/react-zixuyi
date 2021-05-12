675e9f30da45cddf50e84e5ff387c14047ca3da9
DeborL17065 <lop17065@uvg.edu.gt>
2021-05-12 15:02:50 -0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 var mqtt = require('mqtt');
 //my_topic_name = 'your-adafruit-Lop17065/f/feed/Prueba2';
-var client = mqtt.connect('mqtt://io.adafruit.com',{
+var client = mqtt.connect('mqtts://io.adafruit.com',{
   port:8883,
   username: "Lop17065",
   password: "aio_vtPD08JncbXPNF3A5bH80Z49h8Bs"
 });
 
+client.on('connect',() =>{
+  client.subscribe(my_topic_name)
+});
 
+client.on('error',(error) => {
+  console.log('MQTT Client Errored');
+  console.log(error);
+});
+
+client.on('message',function (topic, message){
+  //DO SOME SORT OF THING HERE.
+  console.log(message.toString());
+});
 
 //var adafruitIo = require("adafruit-io");
 
